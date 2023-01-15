ec53400d9d91713dde29d5cce23a186107456cec
dismas <afubwa@gmail.com>
2023-01-15 19:05:30 +0400

    Javascript Array find Method lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // code your solution here
+
+
+const record = [
+{ year: "2018", result: "N/A"},
+{ year: "2017", result: "N/A"},
+{ year: "2016", result: "N/A"},
+{ year: "2015", result: "W"},
+{ year: "2014", result: "N/A"},
+{ year: "2013", result: "L"},
+{ year: "2012", result: "N/A"},
+{ year: "2011", result: "N/A"},
+{ year: "2010", result: "N/A"},
+{ year: "2009", result: "N/A"},
+{ year: "2008", result: "N/A"},
+{ year: "2007", result: "N/A"},
+{ year: "2006", result: "N/A"},
+{ year: "2005", result: "N/A"},
+]
+
+
+function superbowlWin(arrayOfObjects) {
+    let foundItem = arrayOfObjects.find((item) => item.result === "W");
+    return foundItem === undefined ? undefined : foundItem["year"];
+  }
